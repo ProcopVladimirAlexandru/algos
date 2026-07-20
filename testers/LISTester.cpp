278a35c1351b22bb334929d1ab32027b324900b7
Vladimir P <procopvladimir01@gmail.com>
2026-07-20 12:48:50 +0300

    feat: add a few more LISN2 tests

--- testers/LISTester.cpp
+++ testers/LISTester.cpp
@@ -12,8 +12,8 @@ void LISTester::test() {
 
     LISN2 lis_n2("LISN2");
 
-    std::vector<std::string> inputs{"ababczde", "ababczd", "a", "ad", ""};
-    std::vector<std::string> expected_results{"abcde", "abcz", "a", "ad", ""};
+    std::vector<std::string> inputs{"", "ababczde", "ababczd", "a", "ad", "ruacbdzopb"};
+    std::vector<std::string> expected_results{"", "abcde", "abcz", "a", "ad", "abdop"};
     for (long long i = 0; i < inputs.size(); ++i) {
         std::cout << "TEST NUMBER " << i << ": LISN2(\"" << inputs[i] << "\")" << std::endl;
         std::string result_lis = lis_n2.get_lis(inputs[i]);
